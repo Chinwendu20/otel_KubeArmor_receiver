7b28d69eff144ef8eb718d042af45877bcc53613
Ononiwu Maureen <amaka013@gmail.com>
2023-04-04 12:38:12 +0100

    Added test to  codebase

--- log_test.go
+++ log_test.go
@@ -2,7 +2,13 @@ package kubearmor_receiver
 
 import (
 	"context"
+	input_operator "github.com/Chinwendu20/kubearmor_receiver/stanza_input_operator"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/adapter"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/operator"
+	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/confmap/confmaptest"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -19,4 +25,30 @@ func TestLogsReceiverNoErrors(t *testing.T) {
 	assert.NoError(t, lle.Start(context.Background(), componenttest.NewNopHost()))
 
 	assert.NoError(t, lle.Shutdown(context.Background()))
+}
+
+func TestLoadConfig(t *testing.T) {
+	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config.yml"))
+	require.NoError(t, err)
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+
+	sub, err := cm.Sub(component.NewIDWithName(typeStr, "").String())
+	require.NoError(t, err)
+	require.NoError(t, component.UnmarshalConfig(sub, cfg))
+
+	assert.Equal(t, testdataConfigYaml(), cfg)
+}
+
+func testdataConfigYaml() *KubearmorConfig {
+	return &KubearmorConfig{
+		BaseConfig: adapter.BaseConfig{
+			Operators: []operator.Config{},
+		},
+		InputConfig: func() input_operator.Config {
+			c := input_operator.NewConfig()
+			c.LogFilter = "policy"
+			return *c
+		}(),
+	}
 }
---
+++ stanza_input_operator/input_operator_test.go
@@ -0,0 +1,81 @@
+package kubearmor_receiver
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/entry"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/operator"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/testutil"
+)
+
+type fakeKubearmorCmd struct{}
+
+func (f *fakeKubearmorCmd) Start() error {
+	return nil
+}
+
+func (f *fakeKubearmorCmd) StdoutPipe() (io.ReadCloser, error) {
+	response := `{"Timestamp":1680277695,"UpdatedTime":"2023-03-31T15:48:15.817142Z","HostName":"host-name","HostPID":26846,"PPID":14270,"PID":26846,"UID":1000,"Type":"HostLog","Source":"/usr/local/go/bin/go","Operation":"File","Resource":"/home/user/go/pkg/mod/cache/download/sumdb/sum.golang.org/tile/8/1/244","Data":"syscall=SYS_OPENAT fd=-100 flags=O_RDONLY|O_CLOEXEC","Result":"Passed"}
+	`
+	reader := bytes.NewReader([]byte(response))
+	return io.NopCloser(reader), nil
+}
+
+func TestInputKubearmor(t *testing.T) {
+	cfg := NewConfig()
+	cfg.OutputIDs = []string{"output"}
+
+	op, err := cfg.Build(testutil.Logger(t))
+	require.NoError(t, err)
+
+	mockOutput := testutil.NewMockOperator("output")
+	received := make(chan *entry.Entry)
+	mockOutput.On("Process", mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+		received <- args.Get(1).(*entry.Entry)
+	}).Return(nil)
+
+	err = op.SetOutputs([]operator.Operator{mockOutput})
+	require.NoError(t, err)
+
+	op.(*Input).newCmd = func(ctx context.Context) cmd {
+		return &fakeKubearmorCmd{}
+	}
+
+	err = op.Start(testutil.NewMockPersister("test"))
+	require.NoError(t, err)
+	defer func() {
+		require.NoError(t, op.Stop())
+	}()
+
+	expected := map[string]interface{}{
+		"Data":        "syscall=SYS_OPENAT fd=-100 flags=O_RDONLY|O_CLOEXEC",
+		"HostName":    "host-name",
+		"HostPID":     float64(26846),
+		"Operation":   "File",
+		"PID":         float64(26846),
+		"PPID":        float64(14270),
+		"Resource":    "/home/user/go/pkg/mod/cache/download/sumdb/sum.golang.org/tile/8/1/244",
+		"Result":      "Passed",
+		"Source":      "/usr/local/go/bin/go",
+		"UID":         float64(1000),
+		"Type":        "HostLog",
+		"UpdatedTime": "2023-03-31T15:48:15.817142Z",
+	}
+
+	// Json unmarshaller converts large numbers to float64 and that is what is being simulated here
+	expectedTimestamp := time.Unix(0, int64(float64(1680277695)*1000))
+	select {
+	case e := <-received:
+		require.Equal(t, expected, e.Body)
+		require.Equal(t, expectedTimestamp, e.Timestamp)
+	case <-time.After(time.Second):
+		require.FailNow(t, "Timed out waiting for entry to be read")
+	}
+}
